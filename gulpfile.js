8095a45d7fa9453ef8b12d6e91a517a40213d969
Ben Williams <8enwilliams@gmail.com>
2018-08-14 18:35:20 +1000

    xml static build sorta happening

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require('gulp');
 
 var del = require('del');
 var exec = require('child_process').exec;
+var fs = require('fs');
 var mime = require('mime');
+var mkdirp = require('mkdirp');
 var seq = require('run-sequence');
 var xml = require('xmlbuilder');
 var zip = require('gulp-zip');
 
 const SFDX_OPTS = { cwd: 'src' };
-const STATIC_FILES = 'src/main/default/staticresources/app/**/*';
+const STATIC_FILES = 'src/force-app/main/default/staticresources/app/**/*';
 const RELEASE_DIR = 'release';
+const META_XML_PATH = RELEASE_DIR + '/app.resource-meta.xml';
 
 /*
   TODO: 
@@ -30,8 +33,31 @@ gulp.task('build:static', function() {
   //   - create app.resource-meta.xml
   //   - zip and rename to app.resource
 
+  /*
+  <?xml version="1.0" encoding="UTF-8"?>
+  <StaticResource xmlns="http://soap.sforce.com/2006/04/metadata">
+      <contentType>text/plain</contentType>
+      <description>Test Resource</description>
+  </StaticResource>
+  */
 
+  mkdirp(RELEASE_DIR, function (err) {
+    var meta = xml.create('StaticResource', {'xmlns': 'http://soap.sforce.com/2006/04/metadata'});    
+    var files = fs.readdirSync('./' + STATIC_FILES);
+    
+    for (var i in files) {
+      var currentFile = './' + STATIC_FILES + '/' + files[i];
+      var stats = fs.statSync(currentFile);
 
+      if (stats.isFile()) {
+        console.log(currentFile);
+      }
+    }
+    
+    var xmlStr = meta.end({pretty: true, indent: '  '});
+
+    fs.writeFile(META_XML_PATH, xmlStr); 
+  });
 });
 
 gulp.task('sfdx:push', function() {
